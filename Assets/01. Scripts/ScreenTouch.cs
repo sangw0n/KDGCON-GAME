8d3542cc7aeab09741635bd74dea3329a54feece
sangw0n <pilwon070127@naver.com>
2024-08-29 20:36:05 +0900

    추가 | 모니터 터치 시스템

---
+++ Assets/01. Scripts/ScreenTouch.cs
@@ -0,0 +1,105 @@
+// # System
+using System.Collections;
+using System.Collections.Generic;
+
+// # Unity
+using UnityEngine;
+
+// # Project 
+using DG.Tweening;
+
+public class ScreenTouch : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask screenLayer;
+    [SerializeField]
+    private LayerMask offScreenLayer;
+
+    [Header("Zoom Settings")]
+    [SerializeField]
+    private float       zoomViewSize;
+    [SerializeField]
+    private float       zoomingSpeed;
+
+    private Vector3     defaultCameraPosition;
+    private float       defaultCameraViewSize;
+
+    private bool        isZooming;
+    private bool        isZoomIn;
+
+    private void Awake()
+    {
+        defaultCameraPosition = Camera.main.transform.position;
+        defaultCameraViewSize = Camera.main.orthographicSize;
+
+        isZooming             = false;
+        isZoomIn              = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray          = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (!isZooming && (hit.collider == null || hit.transform.CompareTag("Finish")))
+            {
+                isZooming = true;
+                isZoomIn = !isZoomIn;
+
+                ZoomOutScreen();
+            }
+            else if (!isZooming && hit.transform.CompareTag("Screen"))
+            {
+                isZooming = true;
+                isZoomIn = !isZoomIn;
+
+                ZoomInScreen(hit.transform);
+            }
+        }
+    }
+
+    private void ZoomInScreen(Transform detectedScreen)
+    {
+        Vector3 zoomPosition = new Vector3
+        (
+            detectedScreen.position.x, 
+            detectedScreen.position.y, 
+            Camera.main.transform.position.z
+        );
+
+        Sequence sequence = DOTween.Sequence();
+
+        Tween tr0 = Camera.main.transform.DOMove(zoomPosition, zoomingSpeed);
+        Tween tr1 = Camera.main.DOOrthoSize(zoomViewSize, zoomingSpeed);
+
+        sequence.Append(tr0);
+        sequence.Join(tr1);
+
+        sequence.Play().OnComplete(() =>
+        {
+            Camera.main.transform.position = zoomPosition;
+            Camera.main.orthographicSize   = zoomViewSize;
+            isZooming = false;
+        });
+    }
+
+    private void ZoomOutScreen()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        Tween tr0 = Camera.main.transform.DOMove(defaultCameraPosition, zoomingSpeed);
+        Tween tr1 = Camera.main.DOOrthoSize(defaultCameraViewSize, zoomingSpeed);
+
+        sequence.Append(tr0);
+        sequence.Join(tr1);
+
+        sequence.Play().OnComplete(() =>
+        {
+            Camera.main.transform.position = defaultCameraPosition;
+            Camera.main.orthographicSize   = defaultCameraViewSize;
+            isZooming = false;
+        });
+    }
+}
