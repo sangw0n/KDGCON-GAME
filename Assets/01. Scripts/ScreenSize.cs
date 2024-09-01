15a227820cbb5bbe9bec0b253fd6c4befb1ef041
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-09-01 21:01:41 +0900

    추가 | 로고씬

--- Assets/01. Scripts/ScreenSize.cs
+++ Assets/01. Scripts/ScreenSize.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScreenSize : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        SetResolution();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 해상도 고정 함수
+    /// </summary>
+    public void SetResolution()
     {
-        
+        int setWidth = 1920; // 화면 너비
+        int setHeight = 1080; // 화면 높이
+
+        //해상도를 설정값에 따라 변경
+        //3번째 파라미터는 풀스크린 모드를 설정 > true : 풀스크린, false : 창모드
+        Screen.SetResolution(setWidth, setHeight, true);
     }
 }
